7740529339432a71416e086eda5b21f193804fca
taren07 <taro.enchi@gmail.com>
2023-11-14 23:51:12 +0900

    update

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,5 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import Form from "./Form";
+import userEvent from "@testing-library/user-event";
+
+const user = userEvent.setup();
 
 describe("Form", () => {
 	it("The initial state is an empty text.", () => {
@@ -7,5 +10,12 @@ describe("Form", () => {
 		const input = screen.getByPlaceholderText("Enter text");
 		expect(input).toBeInTheDocument();
 		expect(input).toHaveTextContent("");
+	});
+
+	it("The text entered is submitted.", async () => {
+		render(<Form />);
+		const input = screen.getByPlaceholderText("Enter text");
+		await user.type(input, "Test Text");
+		expect(screen.getByDisplayValue("Test Text")).toBeInTheDocument();
 	});
 });
